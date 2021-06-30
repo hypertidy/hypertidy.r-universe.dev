c5796b985a9b34e5db11d393d1d41eb8c02f1655
Michael Sumner <mdsumner@gmail.com>
2021-06-30 14:59:10 +1000

    all

--- R/createpackages.json.R
+++ R/createpackages.json.R
@@ -1,9 +1,17 @@
-d <- tibble::tibble(package = c("vapour", "gdalio", "affinity", "granulated", "cells", "geodist",
-                                "gdalwebsrv", "sfdct", "ncmeta", "tabularaster", "quadmesh",
-                                "ceramic", "anglr", "silicate", "PROJ", "lazyvector", "reproj",
-                                "lazyraster", "textures", "grout",
-                                "area", "decido", "kekule", "unjoin",
-                                "guerrilla", "crsmeta"))
+#x <- system("gh repo list hypertidy --limit 10000", intern = TRUE)
+#y <- unlist(lapply(strsplit(gsub("^hypertidy/", "", x), "\t"), "[", 1))
+d <- tibble::tibble(package = c("activate", "affinity", "anglr", "area", "cartilage",
+                                "cdt", "cells", "ceramic", "clipper", "CMIP5files", "contourPolys",
+                                "crsmeta", "decido", "discrete", "ffraster",  "gdalio",
+                                 "gdalwebsrv", "geodist", "gladr", "granulated", "gridcol",
+                                "grout", "guerrilla",
+                                "kekule",  "lazyraster", "lazyvector", "manifoldr",
+                                 "ncapi", "ncdump", "ncmeta", "pfft",
+                                "PROJ", "quadmesh",  "realm", "rearcut",
+                                "reproj", "scdb", "scgraph", "scspatstat", "sfdct", "silicate",
+                                "sphier", "svgplotr", "tabularaster", "textures", "tidyff",
+                                "unjoin", "vapour", "vtkr")
+)
 d$url <- sprintf("https://github.com/hypertidy/%s", d$package)
 d$subdir <- NA
 jsonlite::write_json(d, 'packages.json', pretty = TRUE)
